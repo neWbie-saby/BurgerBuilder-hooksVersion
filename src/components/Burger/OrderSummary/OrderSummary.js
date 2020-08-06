dec563f7b39fb5038a4c97d25c006bc6d48e9ca3
neWbie-saby <dsabyasachi388@gmail.com>
2020-08-06 20:05:34 +0530

    Change of Modal to PureComponent using LifeCycle method

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,33 +1,39 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import Aux from '../../../hoc/Aux';
 import Button from '../../UI/Button/Button';
 
-const orderSummary = props => {
-    const summary = Object.keys(props.ingredients)
-        .map(key => (
-            <li
-             style={{
-                display: 'block',
-                width: '88%'
-            }}
-             key={key}>
-                {props.ingredients[key]} <span style={{textTransform: 'capitalize'}}>{key}</span>
-            </li>
-        ))
-    return (
-        <Aux>
-            <h3>Your Order</h3>
-            <p>Your scrumptious burger has :</p>
-            <ul>
-                {summary}
-            </ul>
-            <p><strong>Total Amount : Rs. {props.amount.toFixed(2)}</strong></p>
-            <p>Checkout?</p>
-            <Button btnType="Success" clicked={props.continue}>CONTINUE</Button>
-            <Button btnType="Danger" clicked={props.canceled}>CANCEL</Button>
-        </Aux>
-    );
-};
+class OrderSummary extends Component {
+    // componentDidUpdate(){
+    //     console.log("[OrderSummary.js] rendering")
+    // }
 
-export default orderSummary;
+    render(){
+        const summary = Object.keys(this.props.ingredients)
+            .map(key => (
+                <li
+                style={{
+                    display: 'block',
+                    width: '88%'
+                }}
+                key={key}>
+                    {this.props.ingredients[key]} <span style={{textTransform: 'capitalize'}}>{key}</span>
+                </li>
+            ));
+        return (
+            <Aux>
+                <h3>Your Order</h3>
+                <p>Your scrumptious burger has :</p>
+                <ul>
+                    {summary}
+                </ul>
+                <p><strong>Total Amount : Rs. {this.props.amount.toFixed(2)}</strong></p>
+                <p>Checkout?</p>
+                <Button btnType="Success" clicked={this.props.continue}>CONTINUE</Button>
+                <Button btnType="Danger" clicked={this.props.canceled}>CANCEL</Button>
+            </Aux>
+        );
+    }
+}
+
+export default OrderSummary;
